6a55da719986110614e4f26ada1dedc4fab681fa
mostafa mohammed <000madz000@gmail.com>
2024-07-13 23:38:47 +0300

    adding dashboard user blocker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,19 @@
 import { NextIntlClientProvider } from "next-intl";
 import "./globals.css";
 import { getLocale, getMessages } from "next-intl/server";
+import isAdmin from "@/functions/is-admin";
+import { redirect } from "next/navigation";
 if (!global.models) {
     global.models = {} as any;
 }
 
 export default async function RootLayout({ children }: any) {
     const locale = await getLocale();
+    const admin = await isAdmin();
+
+    if (!admin) {
+        redirect("/");
+    }
 
     return (
         <html lang="en">
