c9ae6acc75dbfbbec4dda994612a3cab2af31e76
mostafa mohammed <000madz000@gmail.com>
2024-07-17 00:09:56 +0300

    adding key prop to boxes

--- src/app/_components/posts-boxes-grid.tsx
+++ src/app/_components/posts-boxes-grid.tsx
@@ -5,7 +5,7 @@ import PostBox from "./post-box";
 export default function PostsBoxesGrid({ posts }: { posts: IPost[] }) {
     return (
         <Grid className="grid gap-3 grid-cols-[repeat(auto-fit,minmax(20rem,1fr))]">
-            {posts.map(post => <PostBox post={post} />)}
+            {posts.map(post => <PostBox post={post} key={post.sections[0].body} />)}
         </Grid>
     )
 }
